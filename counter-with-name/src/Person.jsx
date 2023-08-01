3ae4ec00cb2a5934078cb8152b051116190d9677
Chris <cpaterson152@gmail.com>
2023-08-01 16:48:10 -0700

    Link label with input

--- counter-with-name/src/Person.jsx
+++ counter-with-name/src/Person.jsx
@@ -10,7 +10,7 @@ export function Person() {
         <label htmlFor="nameInput">Enter your name:</label>
         <input
           type="text"
-          name="nameInput"
+          id="nameInput"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
