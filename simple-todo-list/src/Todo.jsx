a4561a28206f7ca3cf0b7d5818c83e041851cc0d
Chris <cpaterson152@gmail.com>
2023-08-18 09:38:37 -0700

    Break out todo items into separate component

---
+++ simple-todo-list/src/Todo.jsx
@@ -0,0 +1,18 @@
+export function Todo({ id, name, completed, toggleTodo, deleteTodo }) {
+  return (
+    <li className="list-item">
+      <label className="list-item-label">
+        <input
+          type="checkbox"
+          data-list-item-checkbox
+          checked={completed}
+          onChange={e => toggleTodo(id, e.target.checked)}
+        />
+        <span data-list-item-text>{name}</span>
+      </label>
+      <button data-button-delete onClick={() => deleteTodo(id)}>
+        Delete
+      </button>
+    </li>
+  );
+}
