adc0a2c389c0a7f30f2cf216de1429f2d207a03a
Chris <cpaterson152@gmail.com>
2023-08-17 20:38:48 -0700

    Todo functionality implemented all within App Component

--- simple-todo-list/src/App.jsx
+++ simple-todo-list/src/App.jsx
@@ -1,32 +1,69 @@
 import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [newTodoName, setNewTodoName] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  function updateTodoList() {
+    if (newTodoName === "") return;
+    setTodoList(currentTodos => {
+      return [
+        ...currentTodos,
+        { name: newTodoName, completed: false, id: crypto.randomUUID() },
+      ];
+    });
+    setNewTodoName("");
+  }
+
+  function toggleTodo(todoId, completed) {
+    setTodoList(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === todoId) return { ...todo, completed };
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(todoId) {
+    setTodoList(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== todoId);
+    });
+  }
+
   return (
     <>
       <ul id="list">
-        <li class="list-item">
-          <label class="list-item-label">
-            <input type="checkbox" data-list-item-checkbox />
-            <span data-list-item-text>Item 1</span>
-          </label>
-          <button data-button-delete>Delete</button>
-        </li>
-        <li class="list-item">
-          <label class="list-item-label">
-            <input type="checkbox" checked data-list-item-checkbox />
-            <span data-list-item-text>Item 2</span>
-          </label>
-          <button data-button-delete>Delete</button>
-        </li>
+        {todoList.map(todo => {
+          return (
+            <li key={todo.id} className="list-item">
+              <label className="list-item-label">
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={e => toggleTodo(todo.id, e.target.checked)}
+                  data-list-item-checkbox
+                />
+                <span data-list-item-text>{todo.name}</span>
+              </label>
+              <button onClick={() => deleteTodo(todo.id)} data-button-delete>
+                Delete
+              </button>
+            </li>
+          );
+        })}
       </ul>
 
       <div id="new-todo-form">
-        <label for="todo-input">New Todo</label>
-        <input type="text" id="todo-input" />
-        <button>Add Todo</button>
+        <label htmlFor="todo-input">New Todo</label>
+        <input
+          type="text"
+          id="todo-input"
+          value={newTodoName}
+          onChange={e => setNewTodoName(e.target.value)}
+        />
+        <button onClick={updateTodoList}>Add Todo</button>
       </div>
     </>
   );
 }
-
-export default App;
