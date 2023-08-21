d4f90ed3a8737368b8a5951b75b11578406c0c0d
Chris <cpaterson152@gmail.com>
2023-08-21 13:14:55 -0700

    Implement error handling and aborting second fetch

--- use-fetch-project/src/useFetch.js
+++ use-fetch-project/src/useFetch.js
@@ -1,24 +1,36 @@
 import { useEffect, useState } from "react";
 
-export function useFetch(url) {
-  const [data, setData] = useState();
-  const [isError, setIsError] = useState(false);
+export default function useFetch(url, options = {}) {
+  const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     setData(undefined);
     setIsError(false);
     setIsLoading(true);
-    fetch(url)
-      .then(res => res.json())
-      .then(setData)
-      .catch(() => setIsError(true))
-      .finally(() => setIsLoading(false));
+
+    const controller = new AbortController();
+
+    fetch(url, { signal: controller.signal, ...options })
+      .then(res => {
+        if (res.status === 200) return res.json();
+        return Promise.reject(res);
+      })
+      .then(jsonData => setData(jsonData))
+      .catch(e => {
+        if (e.name === "AbortError") return;
+        setIsError(true);
+      })
+      .finally(() => {
+        if (controller.signal.aborted) return;
+        setIsLoading(false);
+      });
+
+    return () => {
+      controller.abort();
+    };
   }, [url]);
 
-  return {
-    data,
-    isError,
-    isLoading,
-  };
+  return { data, isLoading, isError };
 }
